52e6dc094c3d606f9e7ade3efc5e6fe31b58e1c2
Gabe-Biele <gabriel.biele@gmail.com>
2016-04-14 18:26:22 -0400

    0.1.0e - Cleaning up git; Removed ProjcetCircleage_Data, and broke our ServerResponseHandlers

---
+++ Assets/Scripts/GameWorld/ServerResponseHandlers/RotationUpdateHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sfs2X.Entities.Data;
+using UnityEngine;
+
+namespace Assets.Scripts.GameWorld.ServerResponseHandlers
+{
+    class RotationUpdateHandler : ServerResponseHandler
+    {
+
+        public void HandleResponse(ISFSObject anObjectIn, GameWorldManager ourGWM)
+        {
+            string aCharacterName = anObjectIn.GetUtfString("CharacterName");
+            if(aCharacterName == ourGWM.getLPC().GetName())
+            {
+                return;
+            }
+            else if(ourGWM.getPlayerDictionary().ContainsKey(aCharacterName))
+            {
+                float Rotation = anObjectIn.GetFloat("Rotation");
+                Debug.Log(Rotation);
+                ourGWM.getPlayerDictionary()[aCharacterName].GetComponent<RemotePlayerController>().SetRotation(Rotation);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameWorld/ServerResponseHandlers/SpawnPlayerHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sfs2X.Entities.Data;
+
+namespace Assets.Scripts.GameWorld.ServerResponseHandlers
+{
+    class SpawnPlayerHandler : ServerResponseHandler
+    {
+
+        public void HandleResponse(ISFSObject anObjectIn, GameWorldManager ourGWM)
+        {
+            if(anObjectIn.GetBool("IsLocal"))
+            {
+                ourGWM.spawnLocalPlayer(anObjectIn.GetUtfString("CharacterName"), anObjectIn.GetFloatArray("Location"));
+            }
+            else if(!anObjectIn.GetBool("IsLocal"))
+            {
+                float[] LocationArray = anObjectIn.GetFloatArray("Location");
+                float Rotation = anObjectIn.GetFloat("Rotation");
+                string aCharacterName = anObjectIn.GetUtfString("CharacterName");
+                ourGWM.spawnRemotePlayer(aCharacterName, LocationArray, Rotation);
+            }
+        }
+    }
+}
