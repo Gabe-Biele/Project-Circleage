b8755f52f9c63d2a43806831e9c38a97283ccb3a
McRealz <mccoyrjm@gmail.com>
2016-04-18 20:48:35 -0400

    Issue#15: Chat doesn't send when message is too long
    - clean up code; separated sections into regions
    - added check to not send message if no message was typed in the first
    place

--- Assets/Scripts/GameWorld/GameUI.cs
+++ Assets/Scripts/GameWorld/GameUI.cs
@@ -1,110 +1,131 @@
-﻿using UnityEngine;
+﻿using System;
 using System.Collections;
-using Sfs2X;
+using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using Sfs2X;
+using Sfs2X.Core;
 using Sfs2X.Entities.Data;
 using Sfs2X.Requests;
-using UnityEngine.UI;
-using Sfs2X.Core;
-using System;
-using System.Collections.Generic;
 
 public class GameUI : MonoBehaviour
 {
-    private SmartFox SFServer;
+   private SmartFox SFServer;
 
-    private InputField ChatTB;
-    private Boolean ChatTBisFocused;
-    private string ChatText;
+   private InputField ChatTB;
+   private bool ChatTBisFocused;
+   private string ChatText;
 
-    private GameObject ChatContent;
-    private List<GameObject> ChatTextLabel;
+   private GameObject ChatContent;
+   private List<GameObject> ChatTextLabel;
 
-    private GameObject rayCastLabel;
+   private GameObject rayCastLabel;
 
 
-    // Use this for initialization
-    void Start ()
-    {
-        SFServer = SmartFoxConnection.Connection;
+   // Use this for initialization
+   void Start()
+   {
+      SFServer = SmartFoxConnection.Connection;
 
-        ChatTB = GameObject.Find("ChatTB").GetComponent<InputField>();
-        ChatContent = GameObject.Find("ChatContent");
-        ChatTextLabel = new List<GameObject>();
+      ChatTB = GameObject.Find("ChatTB").GetComponent<InputField>();
+      ChatContent = GameObject.Find("ChatContent");
+      ChatTextLabel = new List<GameObject>();
 
-        rayCastLabel = GameObject.Find("RayCastLabel");
-        rayCastLabel.SetActive(false);
-        ChatTBisFocused = false;
-    }
+      rayCastLabel = GameObject.Find("RayCastLabel");
+      rayCastLabel.SetActive(false);
+      ChatTBisFocused = false;
+   }
 
-    // Update is called once per frame
-    void Update ()
-    {
-        if(this.ChatTB.isFocused)
-        {
-            ChatTBisFocused = true;
-        }
-        if(Input.GetKeyDown(KeyCode.Return))
-        {
-            if(this.ChatTBisFocused)
-            {
-                this.SendMessage();
-                this.ChatTBisFocused = false;
-            }
-            else
-            {
-                this.ChatTB.Select();
-                this.ChatTBisFocused = true;
-            }
-        }
-    }
-    void FixedUpdate()
-    {
-        if(SFServer != null)
-        {
-            SFServer.ProcessEvents();
-        }
-    }
-    public void SendMessage()
-    {
-        this.ChatText = this.ChatTB.text;
-        this.ChatTB.text = "";
+   // Update is called once per frame
+   void Update()
+   {
+      if (this.ChatTB.isFocused)
+      {
+         ChatTBisFocused = true;
+      }
+      if (Input.GetKeyDown(KeyCode.Return))
+      {
+         if (this.ChatTBisFocused)
+         {
+            this.SendMessage();
+            this.ChatTBisFocused = false;
+         }
+         else
+         {
+            this.ChatTB.Select();
+            this.ChatTBisFocused = true;
+         }
+      }
+   }
 
-        ISFSObject ObjectIn = new SFSObject();
-        ObjectIn.PutUtfString("ChatText", this.ChatText);
-        SFServer.Send(new ExtensionRequest("ProcessChat", ObjectIn));
-    }
-    public void QuitButton_Clicked()
-    {
-        Application.Quit();
-    }
+   void FixedUpdate()
+   {
+      if (SFServer != null)
+      {
+         SFServer.ProcessEvents();
+      }
+   }
 
-    public void processChat(String chatText)
-    {
-        ChatTextLabel.Add((GameObject)Instantiate(Resources.Load("UI/ChatText", typeof(GameObject))));
-        ChatTextLabel[ChatTextLabel.Count - 1].name = "ChatText[" + (ChatTextLabel.Count - 1) + "]";
-        ChatTextLabel[ChatTextLabel.Count - 1].GetComponent<RectTransform>().SetParent(this.ChatContent.GetComponent<RectTransform>());
-        ChatTextLabel[ChatTextLabel.Count - 1].GetComponent<Text>().text = chatText;
-        for(int i = 0; i < ChatTextLabel.Count; i++)
-        {
-            ChatTextLabel[i].GetComponent<RectTransform>().localPosition = new Vector3(205, ChatTextLabel[i].GetComponent<RectTransform>().localPosition.y + 18);
-        }
-    }
-    public void activateRayCastLabel(GameObject aGameObject)
-    {
-        if(aGameObject.tag == "Resource")
-        {
-            rayCastLabel.SetActive(true);
-            rayCastLabel.GetComponent<Text>().text = aGameObject.name.Split('_')[1] + "\nPress F to Gather";
-        }
-    }
-    public void deactivateRayCastLabel()
-    {
-        rayCastLabel.SetActive(false);
-    }
+   #region Chat Box
 
-    public bool GetchatTBFocus()
-    {
-        return ChatTB.isFocused;
-    }
+   public void SendMessage()
+   {
+      if (this.ChatTB.text == "") // if no message content, don't bother sending
+      {
+         this.ChatText = this.ChatTB.text;
+         this.ChatTB.text = "";
+
+         ISFSObject ObjectIn = new SFSObject();
+         ObjectIn.PutUtfString("ChatText", this.ChatText);
+         SFServer.Send(new ExtensionRequest("ProcessChat", ObjectIn));
+      }
+   }
+
+   public void processChat(String chatText)
+   {
+      ChatTextLabel.Add((GameObject)Instantiate(Resources.Load("UI/ChatText", typeof(GameObject))));
+      ChatTextLabel[ChatTextLabel.Count - 1].name = "ChatText[" + (ChatTextLabel.Count - 1) + "]";
+      ChatTextLabel[ChatTextLabel.Count - 1].GetComponent<RectTransform>().SetParent(this.ChatContent.GetComponent<RectTransform>());
+      ChatTextLabel[ChatTextLabel.Count - 1].GetComponent<Text>().text = chatText;
+
+      for (int i = 0; i < ChatTextLabel.Count; i++)
+      {
+         ChatTextLabel[i].GetComponent<RectTransform>().localPosition = new Vector3(205, ChatTextLabel[i].GetComponent<RectTransform>().localPosition.y + 18);
+      }
+   }
+
+   public bool GetchatTBFocus()
+   {
+      return ChatTB.isFocused;
+   }
+
+   #endregion
+
+   #region Resource Gathering
+
+   public void activateRayCastLabel(GameObject aGameObject)
+   {
+      if (aGameObject.tag == "Resource")
+      {
+         rayCastLabel.SetActive(true);
+         rayCastLabel.GetComponent<Text>().text = aGameObject.name.Split('_')[1] + "\nPress F to Gather";
+      }
+   }
+   public void deactivateRayCastLabel()
+   {
+      rayCastLabel.SetActive(false);
+   }
+
+   #endregion
+
+   #region Misc
+
+   public void QuitButton_Clicked()
+   {
+      Application.Quit();
+   }
+
+   #endregion
+
 }
