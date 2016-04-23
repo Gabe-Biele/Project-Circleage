9d76814a012fbcd64af65b5a2a03ed6b76a4039d
thecakebandito <mdimil@gmail.com>
2016-04-23 14:58:03 -0400

    Inventory works somewhat
    See problems for problems with the inventory

--- Assets/Scripts/GameWorld/Item.cs
+++ Assets/Scripts/GameWorld/Item.cs
@@ -1,18 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item : MonoBehaviour {
+public class Item : MonoBehaviour
+{
 
-    int ID;
+    public int ID;
     int bagPosition;
-    string name;
+    public int quantity;
+    string itemName;
+    string itemDescription;
     string subLocation;
+    public string picture;
 
-    Item(int itemID, string p_subLocation, int p_bagPosition)
+    public Item(int itemID, string p_subLocation, int p_bagPosition, int p_quantity)
     {
         ID = itemID;
         subLocation = p_subLocation;
         bagPosition = p_bagPosition;
+        quantity = p_quantity;
     }
-	
+    
+    public void setName(string itName)
+    {
+        itemName = itName;
+    }
+    public void setDescription(string itDesc)
+    {
+        itemDescription = itDesc;
+    }
 }
